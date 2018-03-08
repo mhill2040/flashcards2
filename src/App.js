dd402f2c96de3fac775694f67ab00690a4fafa62
Melissa Hill <mhill2040@gmail.com>
2018-03-07 17:22:24 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Flashcards 2</h1>
+        </header>
+        <p className="App-intro">
+          Flashcard Things
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
